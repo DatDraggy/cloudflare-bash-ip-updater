69264650325fb69283c9fc668fad4d37806e9092
DatDraggy <github.com@kieran.de>
2023-04-15 12:44:31 +0200

    Don't try to update IP if record not existing

--- update_ip.sh
+++ update_ip.sh
@@ -3,6 +3,9 @@
 zone_identifier="" #Log in to Cloudflare, select domain, copy "Zone ID" from Overview
 auth_key="" #Below "Zone ID", click "Get your API key" and create a token with the "Edit zone DNS" template. Set your zone in "Zone Resources" and create
 record_name="" #example.com
+
+##########################
+
 ip=$(curl -s v4.ident.me)
 ipSix=$(curl -s v6.ident.me)
 log_file="/var/log/update_ip.log"
@@ -22,26 +25,26 @@ fi
 recordA_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=A" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" | jq '.result[0].id' -r)
 recordAAAA_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=AAAA" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" | jq '.result[0].id' -r)
 #Update when A different
-if [[ $ip != $recordA_ip ]]; then
+if [[ $ip != $recordA_ip && $recordA_ip != "null" ]]; then
     update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$recordA_identifier" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
     if [[ $update == *"\"success\":false"* ]]; then
         message="v4 API UPDATE FAILED. DUMPING RESULTS:\n$update"
         log "$message"
         echo -e "$message"
-        exit 1 
+        exit 1
     else
         message="IPv4 changed to: $ip"
         log "$message"
     fi
 fi
 #Update when AAAA different
-if [[ $ipSix != $recordAAAA_ip ]]; then
+if [[ $ipSix != $recordAAAA_ip && $recordAAAA_ip != "null" ]]; then
     updateSix=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$recordAAAA_identifier" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"AAAA\",\"name\":\"$record_name\",\"content\":\"$ipSix\"}")
     if [[ $updateSix == *"\"success\":false"* ]]; then
         message="v6 API UPDATE FAILED. DUMPING RESULTS:\n$updateSix"
         log "$message"
         echo -e "$message"
-        exit 1 
+        exit 1
     else
         message="IPv6 changed to: $ipSix"
         log "$message"
