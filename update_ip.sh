2777d0f74bc39308ea1505d1e82e811d7141fe05
DatDraggy <github.com@kieran.de>
2023-04-15 12:32:43 +0200

    Use specific token instead of global api key

--- update_ip.sh
+++ update_ip.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-auth_email="" #Account email
 zone_identifier="" #Log in to Cloudflare, select domain, copy "Zone ID" from Overview
-auth_key="" #Below "Zone ID", click "Get your API key" and copy the global key
+auth_key="" #Below "Zone ID", click "Get your API key" and create a token with the "Edit zone DNS" template. Set your zone in "Zone Resources" and create
 record_name="" #example.com
 ip=$(curl -s v4.ident.me)
 ipSix=$(curl -s v6.ident.me)
@@ -13,20 +12,20 @@ log() {
     fi
 }
 
-recordA_ip=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=A" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" | jq '.result[0].content' -r)
-recordAAAA_ip=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=AAAA" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" | jq '.result[0].content' -r)
+recordA_ip=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=A" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" | jq '.result[0].content' -r)
+recordAAAA_ip=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=AAAA" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" | jq '.result[0].content' -r)
 #Stop when same
 if [[ $ip == $recordA_ip ]] && [[ $ipSix == $recordAAAA_ip ]]; then
     exit 0
 fi
 
-recordA_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=A" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" | jq '.result[0].id' -r)
-recordAAAA_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=AAAA" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" | jq '.result[0].id' -r)
+recordA_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=A" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" | jq '.result[0].id' -r)
+recordAAAA_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name&type=AAAA" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" | jq '.result[0].id' -r)
 #Update when A different
 if [[ $ip != $recordA_ip ]]; then
-    update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$recordA_identifier" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
+    update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$recordA_identifier" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
     if [[ $update == *"\"success\":false"* ]]; then
-        message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
+        message="v4 API UPDATE FAILED. DUMPING RESULTS:\n$update"
         log "$message"
         echo -e "$message"
         exit 1 
@@ -37,9 +36,9 @@ if [[ $ip != $recordA_ip ]]; then
 fi
 #Update when AAAA different
 if [[ $ipSix != $recordAAAA_ip ]]; then
-    updateSix=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$recordAAAA_identifier" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"AAAA\",\"name\":\"$record_name\",\"content\":\"$ipSix\"}")
+    updateSix=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$recordAAAA_identifier" -H "Authorization: Bearer $auth_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"AAAA\",\"name\":\"$record_name\",\"content\":\"$ipSix\"}")
     if [[ $updateSix == *"\"success\":false"* ]]; then
-        message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
+        message="v6 API UPDATE FAILED. DUMPING RESULTS:\n$updateSix"
         log "$message"
         echo -e "$message"
         exit 1 
